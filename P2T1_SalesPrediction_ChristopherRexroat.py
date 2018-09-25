4f3e7a28686f5715f2686f1d2cf63d22d683a7ac
rexroatc0852 <43561789+rexroatc0852@users.noreply.github.com>
2018-09-24 23:57:33 -0400

    First submission

---
+++ P2T1_SalesPrediction_ChristopherRexroat.py
@@ -0,0 +1,13 @@
+# Tutorial-P2T1-Sales Prediction
+# 09/23/2018
+# CTI-110 P2T1-Sales Prediction
+# Christopher Rexroat
+ 
+# Get the projected total sales.
+total_sales = float(input('Enter the projected sales: '))
+
+# Calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+# Display the profit.
+print('The profit is $', format(profit, ',.2f'))
